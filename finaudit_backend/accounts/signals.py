b67c2276f86bec3d752eaee378f48e8e925a1601
Daniel Offei <archimedes301@gmail.com>
2026-05-21 20:07:53 +0000

    feat(accounts): add signals.py file responsible for triggering user settings and profile creation upon sign-up

---
+++ finaudit_backend/accounts/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+
+from .models import UserSettings, Profile
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_related_objects(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        UserSettings.objects.create(user=instance)
+
