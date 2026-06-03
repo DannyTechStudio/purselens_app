b51819c8a3992d3e3ffbb400b8d914c52932892a
Daniel Offei <archimedes301@gmail.com>
2026-06-03 23:43:37 +0000

    feat(finances): add validation file to ensure transaction description doesn't exceed 100 words

---
+++ finaudit_backend/finances/validators.py
@@ -0,0 +1,14 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator
+
+
+# Custom validators for enforcing 100 words for transaction description
+def validate_max_words(value):
+    words = value.split()
+    
+    if len(words) > 100:
+        raise ValidationError(
+            "Transaction description cannot be more than 100 words."
+        )
+        
+        
