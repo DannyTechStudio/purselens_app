eb222eaf76514f019447e24dc836150a5f1536c7
Daniel Offei <archimedes301@gmail.com>
2026-06-11 23:22:52 +0000

    refactor(setting): update access token lifetime for a prolonged usage

--- finaudit_backend/finaudit_backend/settings.py
+++ finaudit_backend/finaudit_backend/settings.py
@@ -96,7 +96,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timezone.timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timezone.timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timezone.timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
