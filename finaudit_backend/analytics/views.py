bfe6e2cb1568478be9fef1604ccbaffbcc93d040
Daniel Offei <archimedes301@gmail.com>
2026-06-13 20:54:05 +0000

    refactor(analytics): update 'AnalyticsView' to orchestrates newly refactored service layer using 'AnalyticsService' class

--- finaudit_backend/analytics/views.py
+++ finaudit_backend/analytics/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+
+from .services import AnalyticsService
+from utils.response_helper_methods import success_response
 
 # Create your views here.
+class AnalyticsView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get(self, request):
+        analytics_data = AnalyticsService.get_user_analytics_data(request.user)
+        
+        return success_response(
+            message= "Analytical data retrieved successfully.",
+            data=analytics_data
+        )
