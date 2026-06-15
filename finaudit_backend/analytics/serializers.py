045be55d5ae7288088221b76e6fa9ea7eff62a4d
Daniel Offei <archimedes301@gmail.com>
2026-06-15 03:46:48 +0000

    feat(analytics): add serializers for monthly summary, category breakdown, cashflow, budget overview and performance responsible for enforcing validations

--- finaudit_backend/analytics/serializers.py
+++ finaudit_backend/analytics/serializers.py
@@ -11,10 +11,10 @@ class BudgetOverviewFieldsSerializer(serializers.Serializer):
     total_budget_amount = serializers.DecimalField(max_digits=12, decimal_places=2)
     total_spent = serializers.DecimalField(max_digits=12, decimal_places=2)
     total_remaining = serializers.DecimalField(max_digits=12, decimal_places=2)
-    overall_utilization = serializers.DecimalField(max_digits=12, decimal_places=2)
-    budgets_on_track = serializers.IntegerField(min=0)
-    budget_at_risk = serializers.IntegerField(min=0)
-    budget_exceeded = serializers.IntegerField(min=0)
+    overall_utilization = serializers.DecimalField(max_digits=6, decimal_places=2)
+    budgets_on_track = serializers.IntegerField(min_value=0)
+    budgets_at_risk = serializers.IntegerField(min_value=0)
+    budgets_exceeded = serializers.IntegerField(min_value=0)
 
 class RecentTransactionsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -37,3 +37,31 @@ class AnalyticsOutputSerializer(serializers.Serializer):
     top_categories = TopCategorySerializer(many=True)
     recent_transactions = RecentTransactionsSerializer(many=True)
     insights = InsightCardSerializer(many=True)
+
+class MonthlySummaryItemSerializer(serializers.Serializer):
+    month = serializers.CharField()
+    income = serializers.DecimalField(max_digits=12, decimal_places=2)
+    expense = serializers.DecimalField(max_digits=12, decimal_places=2)
+    balance = serializers.DecimalField(max_digits=12, decimal_places=2)
+
+class CategoryBreakdownSerializer(serializers.Serializer):
+    category_id = serializers.UUIDField()
+    category_name = serializers.CharField()
+    amount = serializers.DecimalField(max_digits=12, decimal_places=2)
+    percentage = serializers.DecimalField(max_digits=6, decimal_places=2)
+
+class CashflowSerializer(serializers.Serializer):
+    month = serializers.CharField()
+    inflow = serializers.DecimalField(max_digits=12, decimal_places=2)
+    outflow = serializers.DecimalField(max_digits=12, decimal_places=2)
+    net_flow = serializers.DecimalField(max_digits=12, decimal_places=2)
+
+class BudgetPerformanceSerializer(serializers.Serializer):
+    category_id = serializers.UUIDField()
+    category_name = serializers.CharField()
+    budget_amount = serializers.DecimalField(max_digits=12, decimal_places=2)
+    spent = serializers.DecimalField(max_digits=12, decimal_places=2)
+    remaining = serializers.DecimalField(max_digits=12, decimal_places=2)
+    utilization = serializers.DecimalField(max_digits=6, decimal_places=2)
+    status = serializers.CharField()
+
